9b7f7a602566ae4e1149f220f8b99513f7caa98c
Oloruntobi1 <blgntobi@gmail.com>
2024-06-18 13:30:16 +0100

    [DOC] improve doc for deref unsafe pointer

--- ll.go
+++ ll.go
@@ -5,8 +5,10 @@ import "unsafe"
 // DerefUnsafePointer is useful for situations where you have
 // determined the address of a variable using the
 // address := unsafe.Pointer(&x) //method (so this line be copied)
-// and you want to know value of that byte.
-// Basically it helps with dereference an unsafe.Pointer type.
+// and you want to know value stored at said address.
+// It can also be called GetUnsafePointerAddressValue.
+// NOTE: Every address points to specifically one byte
+// and one byte of data alone.
 func DerefUnsafePointer(address unsafe.Pointer) byte {
 	return *(*byte)(address)
 }
